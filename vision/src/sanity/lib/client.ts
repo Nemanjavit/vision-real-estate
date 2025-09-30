3c9bfc59a4c1dd64ecee000f25fac484ea57a202
Nemanjavit <vitnemanja3@gmail.com>
2025-09-29 22:24:00 -0600

    change client file

--- vision/src/sanity/lib/client.ts
+++ vision/src/sanity/lib/client.ts
@@ -1,5 +1,4 @@
 import { createClient } from "next-sanity";
-
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
